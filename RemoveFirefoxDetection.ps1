3f5e77b86ef6f1de1bf8fcbe51f35319dba3c227
mswanson515 <mswanson515@gmail.com>
2024-10-16 08:12:00 -0400

    Add files via upload

---
+++ RemoveFirefoxDetection.ps1
@@ -0,0 +1,13 @@
+﻿### Detection script to find Mozilla Firefox installed or not
+
+if(test-path -Path 'C:\Program Files\Mozilla Firefox\firefox.exe')
+{
+    $ver = (Get-Item -Path 'C:\Program Files\Mozilla Firefox\firefox.exe').VersionInfo.FileVersion
+    Write-Output "Firefox version $ver is installed"
+    exit 1
+}
+else
+{
+    Write-Output "No firefox installed"
+    exit 0
+}
---
+++ RemoveFirefoxRemediation.ps1
@@ -0,0 +1,5 @@
+﻿### Remediation script to remove Mozilla Firefox
+
+## Remove Mozilla Firefox
+Start-process -FilePath 'C:\Program Files\Mozilla Firefox\uninstall\helper.exe' -ArgumentList "/S" -Wait
+Write-Output "Firefox uninstalled"
