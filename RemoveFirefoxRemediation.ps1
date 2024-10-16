484e210b85127cb1301a88742febc73a06910243
mswanson515 <mswanson515@gmail.com>
2024-10-16 08:32:46 -0400

    Delete RemoveFirefoxRemediation.ps1

--- RemoveFirefoxRemediation.ps1
+++
@@ -1,5 +0,0 @@
-﻿### Remediation script to remove Mozilla Firefox
-
-## Remove Mozilla Firefox
-Start-process -FilePath 'C:\Program Files\Mozilla Firefox\uninstall\helper.exe' -ArgumentList "/S" -Wait
-Write-Output "Firefox uninstalled"
