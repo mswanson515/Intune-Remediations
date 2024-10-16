9f7b7e00871d30e68756d8631ead71d9829ad681
mswanson515 <mswanson515@gmail.com>
2024-10-16 08:33:11 -0400

    Add files via upload

---
+++ RemoveFirefoxDetection.ps1
@@ -0,0 +1,12 @@
+﻿### Detection script to find Mozilla Firefox installed or not
+
+$firefoxPath = 'C:\Program Files\Mozilla Firefox\firefox.exe'
+
+if (Test-Path -Path $firefoxPath) {
+    $ver = (Get-Item -Path $firefoxPath).VersionInfo.FileVersion
+    Write-Output "Firefox version $ver is installed"
+    exit 1
+} else {
+    Write-Output "No Firefox installed"
+    exit 0
+}
---
+++ RemoveFirefoxRemediation.ps1
@@ -0,0 +1,23 @@
+﻿### Remediation script to remove Mozilla Firefox
+
+# Define the path to the uninstaller
+$uninstallerPath = 'C:\Program Files\Mozilla Firefox\uninstall\helper.exe'
+
+# Check if the uninstaller exists
+if (Test-Path -Path $uninstallerPath) {
+    # Run the uninstaller silently
+    Start-Process -FilePath $uninstallerPath -ArgumentList "/S" -Wait
+    Write-Output "Firefox uninstalled"
+    
+    # Remove Firefox user data
+    Remove-Item -Path "$env:APPDATA\Mozilla\Firefox" -Recurse -Force
+    Remove-Item -Path "$env:LOCALAPPDATA\Mozilla\Firefox" -Recurse -Force
+    
+    # Remove Firefox program files
+    Remove-Item -Path "$env:ProgramFiles\Mozilla Firefox" -Recurse -Force
+    
+    # Remove Firefox from the start menu
+    Remove-Item -Path "$env:ProgramData\Microsoft\Windows\Start Menu\Programs\Firefox.lnk" -Force
+} else {
+    Write-Output "Firefox uninstaller not found"
+}
